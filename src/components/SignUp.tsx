e28ce67f490cfc078e1dd37ac235a8ab7416b561
thumbthing <ilscc0902@gmail.com>
2023-04-19 16:41:24 +0900

    feat: SignUp.tsx 이메일, 비밀번호 유효성 검증

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function SignUp() {
   const [ id, setId ] = useState('');
   const [ password, setPassword ] = useState('');
+  const [ disable , setDisable ] = useState(false);
   const userForm = {
     email: id,
     password: password,
   };
+
+  useEffect(() => {
+    if(id.includes('@') && password.length >= 8) {
+      setDisable(false);
+    } else {
+      setDisable(true);
+    }
+  }, [id, password]);
 
   return (
     <>
@@ -22,7 +31,7 @@ export default function SignUp() {
         <button
           data-testid = "signup-button"
           onClick={ e => console.log("회원가입")}
-          disabled = { false }
+          disabled = { disable }
         >회원 가입</button>
     </>
   )
